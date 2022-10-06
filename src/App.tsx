c78dcd6774d4718de68c4ccdf2f5b010e9987dbb
vadimcrmnl1 <you@example.com>
2022-10-06 16:10:54 +0300

    change README.md App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <Header/>
+    );
 }
 
+const Header = () => {
+    return (
+        <div>
+            <h1>MAIN COURSE</h1>
+            <span><b>0% </b></span>
+            <span><b>10% </b></span>
+            <span>20% </span>
+            <span>40% </span>
+            <span>60% </span>
+            <span>80% </span>
+            <span>100%</span>
+        </div>
+    )
+}
 export default App;
