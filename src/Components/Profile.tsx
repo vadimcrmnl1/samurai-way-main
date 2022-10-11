4beb056b6db3a8eef7d698b4d50e02524aa24ee0
vadimcrmnl1 <you@example.com>
2022-10-11 15:40:02 +0300

    change components

---
+++ src/Components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=2000' alt='Picture'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Profile;
